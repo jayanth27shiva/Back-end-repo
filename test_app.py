95166bb22cebb7b3cde525917f940214343d303a
Jayanth J <83591166+jayanth27shiva@users.noreply.github.com>
2025-03-17 11:02:11 +0530

    initial

--- test_app.py
+++ test_app.py
@@ -4,6 +4,14 @@ import json
 import jwt
 from datetime import datetime, timedelta
 
+import pytest
+from app import app  
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 @pytest.fixture
 def client():
